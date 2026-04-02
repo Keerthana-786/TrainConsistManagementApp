fc048327b28cb7dec60cc2d7ebbd442ef5ca4c32
Keerthana-786 <keerthanar0396@gmail.com>
2026-04-02 13:53:46 +0530

    Base application setup

--- src/TrainConsistManagementApp.java
+++ src/TrainConsistManagementApp.java
@@ -1,14 +1,1 @@
-public class TrainConsistManagementApp {
-    public static void main(String[] args) {
-        try {
-            int capacity = -1;
 
-            if (capacity <= 0) {
-                throw new IllegalArgumentException("Invalid Capacity");
-            }
-
-        } catch (Exception e) {
-            System.out.println("Error: " + e.getMessage());
-        }
-    }
-}
